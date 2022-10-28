2a34aa2e8f72c6d01137db8e39102c582d7f3620
AbdukhamidDeveloper <116890553+AbdukhamidDeveloper@users.noreply.github.com>
2022-10-28 20:48:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const screens = document.querySelectorAll('.screen')
+const screens = document.querySelectorAll('.screen')     
 const startBtn = document.querySelector('#start')
 const res = document.querySelector('.res')
 const board = document.querySelector('.board')
@@ -8,21 +8,23 @@ const ticO = document.querySelector('.ticO')
 const finish = document.querySelector('.finishGame')
 const gameWin = document.querySelector('.gameWin')
 
-const cells = 9
-let x = 0
-let o = 0
-let dr = 0
-let xo = 'x';
-let winner = 0
-let grid = [];
+// Создаем переменные для игры 
 
-startBtn.addEventListener('click', (event) => {
+const cells = 9  // количество клеток 
+let x = 0    // счет для х
+let o = 0    // счет для о
+let dr = 0   // счет для ничьи
+let xo = 'x';   // начальная значение игры
+let winner = 0  // переменная для определения победителя
+let grid = []; // пустой массив для проверки игры
+
+startBtn.addEventListener('click', (event) => {      // кнопка старт
     event.preventDefault()
-    screens[0].classList.add('up')
-    startGame()
+    screens[0].classList.add('up')      
+    startGame()                                 // функция начинает игру
 })
 
-res.addEventListener('click', () => {
+res.addEventListener('click', () => {     // кнопка перезагрузка игры        
     xo = 'x'
     winner = 0
     grid.splice(0)
@@ -31,32 +33,32 @@ res.addEventListener('click', () => {
     startGame()
 })
 
-function startGame() {
+function startGame() {           
     board.append(finish)
 
-    for (i = 0; i < cells; i++) {
+    for (i = 0; i < cells; i++) {    // цикл для создание клетки игры
         const cell = document.createElement('div')
         cell.classList.add('cell')
         board.append(cell)
 
-        let a = grid.push(i)
+        let createArrayforGame = grid.push(i)   // добавление элементов массива
 
-        cell.addEventListener('click', () => {
-            grid[a - 1] = returningXO()
-            cell.classList.add(`${xo}`)
+        cell.addEventListener('click', () => {     // обработка событие для каждой клетки
+            grid[createArrayforGame - 1] = returningXO()         // присваивается массиву значение либо х либо о
+            cell.classList.add(`${xo}`)                     // отображает либо х либо о
             winner++
-            if (testGame(grid)) {
-                finishGame()
-            } else if (winner == 9) {
-                finish.style.display = 'block'
-                dr++
+            if (testGame(grid)) {    //  это условие проверяет закончилось ли игра
+                finishGame()        
+            } else if (winner == 9) {    // определяет ничью если все клетки заполнены
+                viewFinishMessage()
+                dr++    
                 draw.innerHTML = `${dr}`       
             }
         }, { once: true })
     }
 }
 
-function returningXO() {
+function returningXO() {    // по очередно возвращает х и о
     if (xo === "x") {
         xo = "o"
         return xo;
@@ -77,8 +79,8 @@ function testGame(cell) {
     if (cell[2] === cell[4] && cell[4] === cell[6]) return true;
 }
 
-function finishGame() {
-    finish.style.display = 'block'
+function finishGame() {          // определяет победителя
+    viewFinishMessage()
     winner = winner % 2
     if (winner == 0) {
         x++
@@ -87,4 +89,8 @@ function finishGame() {
         o++
         ticO.innerHTML = ` ${o}`
     }
+}
+
+function viewFinishMessage () {    // выводит конец игры
+    finish.style.display = 'block'
 }
